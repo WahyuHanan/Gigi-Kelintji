fab0a98515c430410d33fe6930303663aca77af9
WahyuHanan <wahyuhanan8@gmail.com>
2025-02-21 14:42:57 +0700

    rubah src image hero dan saladpage

--- src/components/SaladPage.jsx
+++ src/components/SaladPage.jsx
@@ -6,10 +6,10 @@ export default function SaladPage() {
           <h1 className="xl:mt-10 text-4xl/loose p-4" data-aos="fade-up">
             MADE BY ORDER!!! <span className="text-pink-400  ">FRESH</span> RIGHT INTO YOUR TUMMY...
           </h1>
-          <img src="public\img\img-ingredients\hand-salad2.png" alt="" className=" translate-y-5 size-max" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-out" data-aos-duration="1000" />
+          <img src="img\img-ingredients\hand-salad2.png" alt="" className=" translate-y-5 size-max" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-out" data-aos-duration="1000" />
         </div>
         <div className="box-image order-1 place-content-center p-4 drop-shadow-xl">
-          <img src="public\img\img-ingredients\GK.jpg" alt="" className="rounded-4xl border-4 border-black w-[550px] mx-auto xl:-mt-20 lg:-mt-25" data-aos="fade-right" />
+          <img src="img\img-ingredients\GK.jpg" alt="" className="rounded-4xl border-4 border-black w-[550px] mx-auto xl:-mt-20 lg:-mt-25" data-aos="fade-right" />
         </div>
       </div>
     </div>
